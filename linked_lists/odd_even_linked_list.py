005df59e6e101378e45d7a217adf87c845fbea09
SeunginLyu <slyu@slyu.local>
2018-05-29 19:18:58 +0900

    Add elegant solution

--- linked_lists/odd_even_linked_list.py
+++ linked_lists/odd_even_linked_list.py
@@ -38,3 +38,22 @@ class Solution:
                 curr = curr.next
             else:
                 return new_head.next
+    // elegant solution
+    def oddEvenList(self, head):
+        odds = ListNode(0)
+        evens = ListNode(0)
+        oddsHead = odds
+        evensHead = evens
+        isOdd = True
+        while head:
+            if isOdd:
+                odds.next = head
+                odds = odds.next
+            else:
+                evens.next = head
+                evens = evens.next
+            isOdd = not isOdd
+            head = head.next
+        evens.next = None
+        odds.next = evensHead.next
+        return oddsHead.next
